0c04ddc15828209bc6c76d3457291ffcd6b0a8b5
fdinocera <38116029+fdinocera@users.noreply.github.com>
2024-11-22 10:07:23 +0100

    aggiunta backend

---
+++ Backend/wpup-backend/src/main/java/com/example/wpup_backend/WpupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.wpup_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpupBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpupBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/wpup-backend/src/test/java/com/example/wpup_backend/WpupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wpup_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpupBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
